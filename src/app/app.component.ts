5868011a22f6e23a7df8776686ee2884a0be67da
Francis <francisnjaramba2@gmail.com>
2024-05-16 15:00:16 +0300

    Laying out the structure html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SubjectsComponent } from "./subjects/subjects.component";
+import { PremiumComponent } from "./premium/premium.component";
+import { NotesComponent } from "./notes/notes.component";
+import { IntroComponent } from "./intro/intro.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, SubjectsComponent, PremiumComponent, NotesComponent, IntroComponent]
 })
 export class AppComponent {
   title = 'dracula';
